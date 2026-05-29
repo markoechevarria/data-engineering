1321d8a9e650c8f29e18a453fc359e196c156fbc
markoechevarria <markofidel10@gmail.com>
2026-05-28 21:23:22 -0500

    feat: project 1

--- 01-foundations/linux-and-bash/the-linux-command-line/projects/1_filesystem_and_navigation_challenges.sh
+++ 01-foundations/linux-and-bash/the-linux-command-line/projects/1_filesystem_and_navigation_challenges.sh
@@ -3,11 +3,29 @@
 ## Create structure
 
 create_structure () {
-    mkdir -p data-lake/{staging,processed,archive,scripts,raw/{sales,users,logs}}
+
+    if [ -d "data-lake" ]; then
+
+        echo "data-lake directory already exists"
+
+    else 
+
+        echo "Creating data-lake directory"
+        mkdir -p data-lake/{staging,processed,archive,scripts,raw/{sales,users,logs}}
+
+    fi
+
 }
 
 create_csvs () {
 
+    for i in {1..100}; do
+
+        date = date
+        echo "$i"
+    done
 }
+
+create_csvs
 
 
