79354834cb9c332a65c2b1be6775afa87c976557
markoechevarria <markofidel10@gmail.com>
2026-06-02 14:04:42 -0500

    feat: linux and bash projects

--- 01-foundations/linux-and-bash/the-linux-command-line/projects/1_filesystem_and_navigation_challenges.sh
+++ 01-foundations/linux-and-bash/the-linux-command-line/projects/1_filesystem_and_navigation_challenges.sh
@@ -2,30 +2,81 @@
 
 ## Create structure
 
+path="$(pwd)/data-lake"
+
 create_structure () {
 
-    if [ -d "data-lake" ]; then
+    if [ -d "$path" ]; then
 
         echo "data-lake directory already exists"
 
     else 
 
         echo "Creating data-lake directory"
-        mkdir -p data-lake/{staging,processed,archive,scripts,raw/{sales,users,logs}}
+        mkdir -p "$path"/{staging,processed,archive,scripts,raw/{sales,users,logs}}
 
     fi
 
 }
 
+delete_structure () {
+
+    echo "Deleting data-lake directory"
+
+    rm -rI "$path"
+}
+
 create_csvs () {
+
+    echo "Creating csvs in /raw/sales"
 
     for i in {1..100}; do
 
-        date = date
-        echo "$i"
+        date=$(date +%d-%m-%y-%H-%M-%S-%3N)
+        touch "$path/raw/sales/$date.csv"
+
     done
 }
 
-create_csvs
+compress_files () {
 
+    echo "Compressing files in /raw/sales"
 
+    tar -czvf sales_compressed.tar.gz "$path/raw/sales"
+
+}
+
+main () {
+
+    option=""
+
+    while [[ $option != "5" ]]
+    do
+
+        echo """
+File Systema dnd Navigation Challenges:
+
+1) Create structure
+2) Delete structure
+3) Create csv
+4) Compress files
+5) Exit
+        """
+        read -p "Select an option: " option
+
+        case "$option" in
+        
+            1) create_structure ;;
+            2) delete_structure ;;
+            3)create_csvs ;;
+            4) compress_files ;;
+            5) option="5";;
+            *) optionn=""
+
+        esac
+
+    done
+
+}
+
+main
--- 01-foundations/linux-and-bash/the-linux-command-line/projects/2_analyze_web_server_logs.sh
+++ 01-foundations/linux-and-bash/the-linux-command-line/projects/2_analyze_web_server_logs.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 # Analyzing Web Server Logs
-#
-# https://raw.githubusercontent.com/elastic/examples/refs/heads/master/Common%20Data%20Formats/nginx_logs/nginx_logs
+
+file="https://raw.githubusercontent.com/elastic/examples/refs/heads/master/Common%20Data%20Formats/nginx_logs/nginx_logs"
+
+download_file () {
+    
+    mkdir "$(pwd)/data"
+
+    curl -o data/nginx_logs $file
+
+}
+
